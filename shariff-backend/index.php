466f7e66e53a21d441202edae3382464d04a4b67
root <root@journals.bib.uni-mannheim.de>
2018-01-22 16:17:54 +0100

    shariff domains

---
+++ shariff-backend/index.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+use Heise\Shariff\Backend;
+
+/**
+ * Demo Application using Shariff Backend
+ */
+class Application
+{
+    /**
+     * Sample configuration
+     *
+     * @var array
+     */
+    private static $configuration = [
+        'cache' => [
+            'ttl' => 60
+        ],
+        'domains' => [
+            'journals.bib.uni-mannheim.de/ubdemo',
+            'journals.bib.uni-mannheim.de/diskurse-digital'
+        ],
+        'services' => [
+            'GooglePlus',
+            'Facebook',
+            'LinkedIn',
+            'Reddit',
+            'StumbleUpon',
+            'Flattr',
+            'Pinterest',
+            'Xing',
+            'AddThis'
+        ]
+    ];
+
+    public static function run()
+    {
+        header('Content-type: application/json');
+
+        $url = isset($_GET['url']) ? $_GET['url'] : '';
+        if ($url) {
+            $shariff = new Backend(self::$configuration);
+            echo json_encode($shariff->get($url));
+        } else {
+            echo json_encode(null);
+        }
+    }
+}
+
+Application::run();
